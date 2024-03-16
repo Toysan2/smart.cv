79284eb148010df80f0591700221472b2836e267
Toy Santana António <toysan3@hotmail.com>
2024-03-16 21:20:15 +0100

    Update dbConnect.js

--- smart.cv/utils/dbConnect.js
+++ smart.cv/utils/dbConnect.js
@@ -10,10 +10,8 @@ async function dbConnect() {
 
   // Use the environment variable to store the MongoDB URL
   const db = await mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true, // Use the new URL parser
-    useUnifiedTopology: true, // Use the new engine to discover and monitor servers
     socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-    connectTimeoutMS: 10000, // Timeout for initial connection attempts
+    connectTimeoutMS: 45000, // Timeout for initial connection attempts
   });
 
   connection.isConnected = db.connections[0].readyState;
