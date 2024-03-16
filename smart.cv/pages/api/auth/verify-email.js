5a2474d49cd20623e9af43ff4fbb29c0a69a7d61
Toy Santana António <toysan3@hotmail.com>
2024-03-16 19:20:05 +0100

    Update verify-email.js

--- smart.cv/pages/api/auth/verify-email.js
+++ smart.cv/pages/api/auth/verify-email.js
@@ -3,30 +3,25 @@ import User from "@/model/user";
 
 export default async function verifyEmail(req, res) {
   await dbConnect();
-
   if (req.method !== "GET") {
     return res
       .status(405)
       .json({ message: "Method not allowed, only GET requests are accepted." });
   }
-
   try {
     const { token } = req.query;
     if (!token) {
       return res.status(400).json({ message: "Missing token." });
     }
-
     const user = await User.findOne({ verificationToken: token });
     if (!user) {
       return res
         .status(404)
         .json({ message: "User not found or already verified." });
     }
-
     user.verified = true;
     user.verificationToken = undefined;
     await user.save();
-
     res.writeHead(302, { Location: "/verified" });
     res.end();
   } catch (error) {
