9e91691a74f0e853b9919167f5197f580b4b109a
Toy Santana António <toysan3@hotmail.com>
2024-03-17 11:15:20 +0100

    MongoDB
    * Set manual indexes in order to reduce response time.

--- smart.cv/model/user.js
+++ smart.cv/model/user.js
@@ -8,18 +8,21 @@ const UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: [true, "Please provide an email"],
-    unique: true,
+    unique: true, // This implicitly creates an index on the email field
   },
   password: {
     type: String,
     required: [true, "Please provide a password"],
   },
-  verificationToken: String, // for email verification
+  verificationToken: String,
   verified: {
     type: Boolean,
     default: false,
   },
   // Add any additional fields as needed
 });
+
+// Explicitly creating an index on the email field for better query performance
+UserSchema.index({ email: 1 });
 
 export default mongoose.models.User || mongoose.model("User", UserSchema);
